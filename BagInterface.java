07b34b87a41bcd433fc58e9cf15cc98e4bcba6dc
janetscho <98375156+janetscho@users.noreply.github.com>
2022-02-07 20:37:43 -0800

    did some updates

--- BagInterface.java
+++ BagInterface.java
@@ -1,6 +1,13 @@
 
-public interface BagInterface <Item>{
-	public BagInterface <Item> union (BagInterface<Item> bag);
-	public BagInterface <Item> intersection(BagInterface<Item> bag);
-	public BagInterface <Item> difference(BagInterface<Item> bag);
+public interface BagInterface <T>{
+	public BagInterface <T> union (BagInterface<T> bag);
+	public BagInterface <T> intersection(BagInterface<T> bag);
+	public BagInterface <T> difference(BagInterface<T> bag);
+	
+	///*
+	public int getSize();
+	public boolean full();
+	public void clear();
+
+	//*/
 }
--- ResizableArrayBag.java
+++ ResizableArrayBag.java
@@ -1,1 +1,20 @@
 
+public class ResizableArrayBag<T> implements BagInterface<T> {
+	public BagInterface <T> union (BagInterface<T> bag) {
+		return null;
+	}
+	public BagInterface <T> intersection(BagInterface<T> bag) {
+		return null;
+	}
+	public BagInterface <T> difference(BagInterface<T> bag) {
+		return null;
+	}
+	public int getSize() {
+		return 0;
+	}
+	public boolean full() {
+		return false;
+	}
+	public void clear() {
+	}
+}
