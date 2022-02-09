5a3d371782473c763ce879cfee05c7a9ed2289bd
Kelly Tsai <kellytsai1997@gmail.com>
2022-02-09 00:56:29 -0800

    updated ArrayBagTest

--- ArrayBagTest.java
+++ ArrayBagTest.java
@@ -1,2 +1,16 @@
-//test
-//test2?
+import java.util.*;
+import java.util.Arrays; 
+
+//contains main method to test the three methods
+public class ArrayBagTest {
+	public static void main (String args[]) {
+		BagInterface <String> bagA = new ResizableArrayBag.java <>; 
+		BagInterface <String>  bagB = new ResizableArrayBag.java <>; 
+		
+		bagA.add("1"); 
+		
+	}
+	
+	
+}
+
