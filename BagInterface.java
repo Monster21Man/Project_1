b019e74d1f7cb8910903ac5af976373e9c638115
Logan <lb97189@gmail.com>
2022-02-09 00:31:49 -0800

    Updated LinkedBag and BagInterface

--- BagInterface.java
+++ BagInterface.java
@@ -1,6 +1,17 @@
 
-public interface BagInterface <Item>{
-	public BagInterface <Item> union (BagInterface<Item> bag);
-	public BagInterface <Item> intersection(BagInterface<Item> bag);
-	public BagInterface <Item> difference(BagInterface<Item> bag);
+public interface BagInterface <T>{
+ public BagInterface <T> union (BagInterface<T> newBag);
+ public BagInterface <T> intersection(BagInterface<T> newBag);
+ public BagInterface <T> difference(BagInterface<T> newBag);
+ public int getCSize();
+ public int getFreq(T anEntry);
+ public boolean isEmpty();
+ public boolean add(T newEntry);
+ public T remove();
+ public boolean remove(T anEntry);
+ public void clear();
+ public boolean contains(T anEntry);
+ public T[] toArray();
+ 
+  
 }
--- ResizableArrayBag.java
+++ ResizableArrayBag.java
@@ -1,1 +1,3 @@
-
+public class ResizableArrayBag implements BagInterface<Item> {
+  
+}
