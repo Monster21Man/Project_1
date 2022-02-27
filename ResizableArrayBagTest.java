d64a19054640db0ade59d25b350065f5ba16447c
janetscho <98375156+janetscho@users.noreply.github.com>
2022-02-26 22:49:53 -0800

    teeheehoo

--- ResizableArrayBagTest.java
+++ ResizableArrayBagTest.java
@@ -4,61 +4,132 @@ public class ResizableArrayBagTest {
 	public static void main(String[] args) {
 		BagInterface<String> bag1 = new ResizableArrayBag<>();
 		BagInterface<String> bag2 = new ResizableArrayBag<>();
-		/*
-		 * // Add 4 entries to bag 1 bag1.add("F"); bag1.add("B"); bag1.add("E");
-		 * bag1.add("D");
-		 * 
-		 * // Add 4 entries to bag 2 bag2.add("D"); bag2.add("C"); bag2.add("B");
-		 * bag2.add("A");
-		 * 
-		 * // Test three methods on the two bags
-		 * System.out.println(Arrays.toString(bag1.union(bag2).toArray()));
-		 * System.out.println(Arrays.toString(bag1.intersection(bag2).toArray()));
-		 * System.out.println(Arrays.toString(bag1.difference(bag2).toArray()));
-		 * 
-		 * // Make a space between two results System.out.println();
-		 * 
-		 * // Clear both bags of all entries bag1.clear(); bag2.clear();
-		 * 
-		 * // Test Case: Bags of unequal size bag1.add("A"); bag1.add("C");
-		 * 
-		 * bag2.add("C"); bag2.add("D"); bag2.add("E");
-		 * 
-		 * // Test our three methods on the two bags
-		 * System.out.println(Arrays.toString(bag1.union(bag2).toArray()));
-		 * System.out.println(Arrays.toString(bag1.intersection(bag2).toArray()));
-		 * System.out.println(Arrays.toString(bag1.difference(bag2).toArray()));
-		 * 
-		 * // Clear both bags of all entries bag1.clear(); bag2.clear();
-		 * 
-		 * // Make a space between two results System.out.println();
-		 * 
-		 * // Test Case: One bag is empty bag1.add(""); bag1.add(""); bag1.add("");
-		 * 
-		 * bag2.add("C"); bag2.add("D"); bag2.add("E");
-		 * 
-		 * // Test our three methods on the two bags
-		 * System.out.println(Arrays.toString(bag1.union(bag2).toArray()));
-		 * System.out.println(Arrays.toString(bag1.intersection(bag2).toArray()));
-		 * System.out.println(Arrays.toString(bag1.difference(bag2).toArray()));
-		 * 
-		 * // Clear both bags of all entries bag1.clear(); bag2.clear();
-		 * 
-		 * // Make a space between two results System.out.println();
-		 * 
-		 * // Test Case: Both bags are empty bag1.add(""); bag1.add(""); bag1.add("");
-		 * 
-		 * bag2.add(""); bag2.add(""); bag2.add("");
-		 * 
-		 * // Test our three methods on the two bags
-		 * System.out.println(Arrays.toString(bag1.union(bag2).toArray()));
-		 * System.out.println(Arrays.toString(bag1.intersection(bag2).toArray()));
-		 * System.out.println(Arrays.toString(bag1.difference(bag2).toArray()));
-		 * 
-		 * // Clear both bags of all entries bag1.clear(); bag2.clear();
-		 * 
-		 * // Make a space between two results System.out.println();
-		 */
+		// Add 4 entries to bag 1
+		bag1.add("F");
+		bag1.add("B");
+		bag1.add("E");
+		bag1.add("D");
+
+		// Add 4 entries to bag 2
+		bag2.add("D");
+		bag2.add("C");
+		bag2.add("B");
+		bag2.add("A");
+
+		// Test three methods on the two bags
+		System.out.print("Bag1: ");
+		System.out.println(Arrays.toString(bag1.toArray()));
+		System.out.print("Bag2: ");
+		System.out.println(Arrays.toString(bag2.toArray()));
+
+		System.out.print("Union: ");
+		System.out.println(Arrays.toString(bag1.union(bag2).toArray()));
+
+		System.out.print("Intersection: ");
+		System.out.println(Arrays.toString(bag1.intersection(bag2).toArray()));
+
+		System.out.print("Difference: ");
+		System.out.println(Arrays.toString(bag1.difference(bag2).toArray()));
+
+		// Make a space between two results
+		System.out.println();
+
+		// Clear both bags of all entries
+		bag1.clear();
+		bag2.clear();
+
+		// Test Case: Bags of unequal size
+		bag1.add("A");
+		bag1.add("C");
+
+		bag2.add("C");
+		bag2.add("D");
+		bag2.add("E");
+
+		// Test our three methods on the two bags
+		System.out.print("Bag1: ");
+		System.out.println(Arrays.toString(bag1.toArray()));
+		System.out.print("Bag2: ");
+		System.out.println(Arrays.toString(bag2.toArray()));
+
+		System.out.print("Union: ");
+		System.out.println(Arrays.toString(bag1.union(bag2).toArray()));
+
+		System.out.print("Intersection: ");
+		System.out.println(Arrays.toString(bag1.intersection(bag2).toArray()));
+
+		System.out.print("Difference: ");
+		System.out.println(Arrays.toString(bag1.difference(bag2).toArray()));
+
+		// Clear both bags of all entries
+		bag1.clear();
+		bag2.clear();
+
+		// Make a space between two results
+		System.out.println();
+
+		// Test Case: One bag is empty
+		bag1.add("");
+		bag1.add("");
+		bag1.add("");
+
+		bag2.add("C");
+		bag2.add("D");
+		bag2.add("E");
+
+		// Test our three methods on the two bags
+		System.out.print("Bag1: ");
+		System.out.println(Arrays.toString(bag1.toArray()));
+		System.out.print("Bag2: ");
+		System.out.println(Arrays.toString(bag2.toArray()));
+
+		System.out.print("Union: ");
+		System.out.println(Arrays.toString(bag1.union(bag2).toArray()));
+
+		System.out.print("Intersection: ");
+		System.out.println(Arrays.toString(bag1.intersection(bag2).toArray()));
+
+		System.out.print("Difference: ");
+		System.out.println(Arrays.toString(bag1.difference(bag2).toArray()));
+
+		// Clear both bags of all entries
+		bag1.clear();
+		bag2.clear();
+
+		// Make a space between two results
+		System.out.println();
+
+		// Test Case: Both bags are empty
+		bag1.add("");
+		bag1.add("");
+		bag1.add("");
+
+		bag2.add("");
+		bag2.add("");
+		bag2.add("");
+
+		// Test our three methods on the two bags
+		System.out.print("Bag1: ");
+		System.out.println(Arrays.toString(bag1.toArray()));
+		System.out.print("Bag2: ");
+		System.out.println(Arrays.toString(bag2.toArray()));
+
+		System.out.print("Union: ");
+		System.out.println(Arrays.toString(bag1.union(bag2).toArray()));
+
+		System.out.print("Intersection: ");
+		System.out.println(Arrays.toString(bag1.intersection(bag2).toArray()));
+
+		System.out.print("Difference: ");
+		System.out.println(Arrays.toString(bag1.difference(bag2).toArray()));
+
+		// Clear both bags of all entries
+		bag1.clear();
+		bag2.clear();
+
+		// Make a space between two results
+		System.out.println();
+
 		// Test Case: Both bags are empty
 		bag1.add("");
 		bag1.add("");
@@ -69,8 +140,19 @@ public class ResizableArrayBagTest {
 		bag2.add(null);
 
 		// Test our three methods on the two bags
+		System.out.print("Bag1: ");
+		System.out.println(Arrays.toString(bag1.toArray()));
+		System.out.print("Bag2: ");
+		System.out.println(Arrays.toString(bag2.toArray()));
+
+		System.out.print("Union: ");
 		System.out.println(Arrays.toString(bag1.union(bag2).toArray()));
-		// System.out.println(Arrays.toString(bag1.intersection(bag2).toArray()));
+
+		System.out.println("woohooooooooooooooo");
+		System.out.print("Intersection: ");
+		System.out.println(Arrays.toString(bag1.intersection(bag2).toArray()));
+
+		System.out.print("Difference: ");
 		System.out.println(Arrays.toString(bag1.difference(bag2).toArray()));
 
 		// Clear both bags of all entries
