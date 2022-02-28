4e94fbbb9544dd082d34b5a92345f2055815af42
Kelly Tsai <kellytsai1997@gmail.com>
2022-02-27 21:23:51 -0800

    added more changes to bagdriver

--- bagDriver.java
+++ bagDriver.java
@@ -129,9 +129,95 @@ public class bagDriver {
 		System.out.println("\nThe difference of your entire order is: "); 
 		System.out.print(Arrays.toString(bag3.difference(bag4).toArray()));
 
-		s.close(); 
+		
+		
+		
+		boolean ask = true;
+		while(ask) {
+			System.out.println("You can now remove or add more items."); 
+			System.out.println("Which bag would you like to change?");
+			System.out.println("Enter 1 for bag3 and 2 for bag4. Enter 9 to quit."); 
+			int choice = s.nextInt(); 
+			if (choice == 1) {
+				System.out.println("Press 1 to remove and 2 to add an item.");
+				int answer = s.nextInt(); 
+				if (answer == 1) {
+					System.out.println("Choose an existing drink to remove.");
+					String removeDrink = s.nextLine(); 
+					removeDrink = s.nextLine(); 
+					bag3.remove(removeDrink); 
+					displayBag(bag3); 
+					System.out.println("If you would like to quit, please enter 9. Press any number to continue.");
+					int quit = s.nextInt();
+					if(quit == 9) {
+						break;
+				}
+			}
+				
+				if (answer == 2) {
+					System.out.println("Type an item to add."); 
+					String addDrink = s.nextLine(); 
+					addDrink = s.nextLine(); 
+					bag3.add(addDrink); 
+					displayBag(bag3);
+					System.out.println("If you would like to quit, please enter 9.");
+					
 
-	}
+				}
+			
+			else if (choice == 2) {
+				System.out.println("Press 1 to remove and 2 to add an item. Press 9 to quit.");
+				answer = s.nextInt(); 
+				if (answer == 1) {
+					System.out.println("Choose an existing food item to remove.");
+					String removeFood = s.nextLine(); 
+					removeFood = s.nextLine(); 
+					bag4.remove(removeFood); 
+					displayBag(bag4); 
+					}
+
+				
+				if (answer == 2) {
+					System.out.println("Type an item to add."); 
+					String addFood = s.nextLine();
+					addFood = s.nextLine();  
+					bag4.add(addFood); 
+					displayBag(bag4);
+					System.out.println("If you would like to quit, please enter 9.");
+					int quit = s.nextInt(); 
+					if(quit == 9) {
+						continue; 
+					}
+					else{
+
+					}
+
+				}
+			}
+				
+
+				}
+		System.out.print(Arrays.toString(bag3.union(bag4).toArray()));
+		System.out.print(Arrays.toString(bag3.intersection(bag4).toArray()));
+		System.out.print(Arrays.toString(bag3.difference(bag4).toArray()));
+			}
+		
+		s.close();
+		}
+			
+
+		
+		
+
+
+	private static void displayBag(BagInterface<String> aBag) {
+		System.out.println("The bag contains " + aBag.getCurrentSize() + " string(s), as follows: ");
+		Object[] bagArray = aBag.toArray(); 
+		for(int index = 0; index < bagArray.length; index++) {
+		  System.out.print(bagArray[index] + " "); 
+		}//end for 
+		System.out.println(); 
+	  }//end displayBag
 
 	
 	
